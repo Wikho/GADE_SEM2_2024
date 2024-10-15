52fc849b23565c19b32a64d340972b78fe283729
Malcom Thonger <malcozui@gmail.com>
2024-10-15 09:38:08 +0200

    Balancing

--- Assets/Scripts/Managers/ClickToSpawnManager.cs
+++ Assets/Scripts/Managers/ClickToSpawnManager.cs
@@ -44,9 +44,9 @@ public class ClickToSpawnManager : MonoBehaviour
     [SerializeField] private LayerMask _raycastLayerMask;
     [SerializeField] private ClickMode m_currentClickMode = ClickMode.None;
     [SerializeField] private bool _printDebug = false;
-    [SerializeField] public int totalBuildingsBuild = 0;
-    [SerializeField] public int totalBuildingsUpgraded = 0;
-    [SerializeField] public int totalResourceBuildings = 0;
+    public int totalBuildingsBuild = 0;
+    public int totalBuildingsUpgraded = 0;
+    public int totalResourceBuildings = 0;
 
     [Header("Upgrade Cost")]
     [SerializeField] private int upgradeWoodCost;
